4202c1fa0164db836903883ae9f2d64b6e4aa723
Aby Abraham <abyabrkal@gmail.com>
2018-12-29 15:22:06 +0400

    Added routes for static direct access

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 module.exports = {
     exportPathMap: function () {
       return {
-        '/': { page: '/' }
+        '/': { page: '/' },
+        '/about': { page: '/about' },
+        '/b/hello-nextjs': { page: '/blog', query: { title: 'Hello Next.js' } },
+        '/b/learn-nextjs': { page: '/blog', query: { title: 'Learn Next.js is awesome' } },
+        '/b/deploy-nextjs': { page: '/blog', query: { title: 'Deploy apps with Zeit' } }
       }
     }
-  }
-  
